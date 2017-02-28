8b5c8ca2713c90c8db8cf49efaa2b86ec318c517
Matthias Beyer <mail@beyermatthias.de>
2017-02-28 14:35:56 +0100

    Add GPL2 header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+//   git-dit - the distributed issue tracker for git
+//   Copyright (C) 2016 Matthias Beyer <mail@beyermatthias.de>
+//   Copyright (C) 2016 Julian Ganz <neither@nut.email>
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License version 2 as
+//   published by the Free Software Foundation.
+//
+
 #[macro_use] extern crate log;
 #[macro_use] extern crate clap;
 extern crate git2;
