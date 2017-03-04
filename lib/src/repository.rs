432a5379fc7546c96f32c053ecef341b4ef5fcbd
Julian Ganz <neither@nut.email>
2017-03-04 03:28:03 +0100

    Add function for finding the initial commit for an issue

--- lib/src/repository.rs
+++ lib/src/repository.rs
@@ -6,7 +6,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-use git2::{Error, Oid, References, Repository};
+use git2::{Commit, Error, Oid, References, Repository};
+use first_parent_iter::FirstParentIter;
 
 
 pub trait RepositoryExt {
@@ -16,12 +17,30 @@ pub trait RepositoryExt {
     /// provided.
     ///
     fn get_issue_heads(&self, issue: Oid) -> Result<References, Error>;
+
+    /// Find the initial message of an issue
+    ///
+    /// For a given message of an issue, find the initial message.
+    ///
+    fn find_tree_init<'a>(&'a self, commit: Commit<'a>) -> Result<Commit, Error>;
 }
 
 impl RepositoryExt for Repository {
     fn get_issue_heads(&self, issue: Oid) -> Result<References, Error> {
         let glob = format!("**/dit/{}/head", issue);
         self.references_glob(&glob)
+    }
+
+    fn find_tree_init<'a>(&'a self, commit: Commit<'a>) -> Result<Commit, Error> {
+        // follow the chain of first parents towards an initial message for
+        // which a head exists
+        for c in FirstParentIter::new(commit) {
+            if try!(self.get_issue_heads(c.id())).count() > 0 {
+                return Ok(c);
+            }
+        }
+
+        Err(Error::from_str("No issue head found"))
     }
 }
 
