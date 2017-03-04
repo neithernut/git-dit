016d2f32ea7df74371cfaad888f001e212a136f6
Julian Ganz <neither@nut.email>
2017-03-04 03:24:19 +0100

    Use import instead of referring to git2::Repository directly
    We will use a few packages, after all.

--- lib/src/repository.rs
+++ lib/src/repository.rs
@@ -6,10 +6,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
+use git2::Repository;
+
 
 pub trait RepositoryExt {
 }
 
-impl RepositoryExt for git2::Repository {
+impl RepositoryExt for Repository {
 }
 
