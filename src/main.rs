12930e44848182dc84706bde4ea50722a0f0e41a
Julian Ganz <neither@nut.email>
2017-03-04 14:12:42 +0100

    Add implementation of find-tree-init-hash subcommand

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ mod error;
 
 use clap::App;
 use git2::Repository;
+use libgitdit::repository::RepositoryExt;
 
 use error::ErrorKind as EK;
 use error::*;
@@ -30,6 +31,19 @@ use error::*;
 fn open_dit_repo() -> Result<Repository> {
     // TODO: access the config and maybe return another repo instead
     Repository::open_from_env().chain_err(|| EK::WrappedGitError)
+}
+
+
+/// find-tree-init-hash subcommand implementation
+///
+fn find_tree_init_hash(repo: &Repository, matches: &clap::ArgMatches) -> i32 {
+    // note: commit is always present since it is a required parameter
+    repo.revparse_single(matches.value_of("commit").unwrap())
+        .and_then(|obj| repo.find_commit(obj.id()))
+        .chain_err(|| EK::WrappedGitError)
+        .and_then(|commit| repo.find_tree_init(commit).chain_err(|| EK::WrappedGitDitError))
+        .map(|commit| {println!("{}", commit.id()); 0})
+        .unwrap_or_else(|err| {error!("{}", err); 1})
 }
 
 
