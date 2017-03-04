e769f31094acd743c0bd3b787cdd99a3b1a35719
Julian Ganz <neither@nut.email>
2017-03-04 03:27:17 +0100

    Add function for finding heads for an existing issue

--- lib/src/repository.rs
+++ lib/src/repository.rs
@@ -6,12 +6,22 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-use git2::Repository;
+use git2::{Error, Oid, References, Repository};
 
 
 pub trait RepositoryExt {
+    /// Get possible heads of an issue by its oid
+    ///
+    /// Returns heads from both the local repository and remotes for the issue
+    /// provided.
+    ///
+    fn get_issue_heads(&self, issue: Oid) -> Result<References, Error>;
 }
 
 impl RepositoryExt for Repository {
+    fn get_issue_heads(&self, issue: Oid) -> Result<References, Error> {
+        let glob = format!("**/dit/{}/head", issue);
+        self.references_glob(&glob)
+    }
 }
 
