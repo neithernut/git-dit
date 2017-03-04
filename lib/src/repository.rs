8c3b677fe55b293b2a123a978bd284517275e712
Matthias Beyer <mail@beyermatthias.de>
2017-03-04 12:17:08 +0100

    Use own error module

--- lib/src/repository.rs
+++ lib/src/repository.rs
@@ -6,7 +6,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-use git2::{Commit, Error, Oid, References, Repository};
+
+use error::ErrorKind as EK;
+use error::*;
+
+use git2::{Commit, Oid, References, Repository};
 use first_parent_iter::FirstParentIter;
 
 
@@ -16,31 +20,37 @@ pub trait RepositoryExt {
     /// Returns heads from both the local repository and remotes for the issue
     /// provided.
     ///
-    fn get_issue_heads(&self, issue: Oid) -> Result<References, Error>;
+    fn get_issue_heads(&self, issue: Oid) -> Result<References>;
 
     /// Find the initial message of an issue
     ///
     /// For a given message of an issue, find the initial message.
     ///
-    fn find_tree_init<'a>(&'a self, commit: Commit<'a>) -> Result<Commit, Error>;
+    fn find_tree_init<'a>(&'a self, commit: Commit<'a>) -> Result<Commit>;
 }
 
 impl RepositoryExt for Repository {
-    fn get_issue_heads(&self, issue: Oid) -> Result<References, Error> {
+    fn get_issue_heads(&self, issue: Oid) -> Result<References> {
         let glob = format!("**/dit/{}/head", issue);
         self.references_glob(&glob)
+            .chain_err(|| EK::WrappedGitError)
     }
 
-    fn find_tree_init<'a>(&'a self, commit: Commit<'a>) -> Result<Commit, Error> {
+    fn find_tree_init<'a>(&'a self, commit: Commit<'a>) -> Result<Commit> {
         // follow the chain of first parents towards an initial message for
         // which a head exists
+        let cid = commit.id();
         for c in FirstParentIter::new(commit) {
-            if try!(self.get_issue_heads(c.id())).count() > 0 {
+            let head = try!(self
+                            .get_issue_heads(c.id())
+                            .chain_err(|| EK::CannotFindIssueHead(c.id())));
+
+            if head.count() > 0 {
                 return Ok(c);
             }
         }
 
-        Err(Error::from_str("No issue head found"))
+        Err(Error::from_kind(EK::NoTreeInitFound(cid)))
     }
 }
 
