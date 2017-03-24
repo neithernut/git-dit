992567848dc94e2bdfe0a34611beaf881ed8bf9d
Julian Ganz <neither@nut.email>
2017-03-24 23:00:25 +0100

    Generalize editor Command building and add a pager function

--- src/programs.rs
+++ src/programs.rs
@@ -9,21 +9,70 @@
 
 use std::process::Command;
 use std::env::var as env_var;
+use std::slice;
 
 use git2::Config;
 
 use error::*;
 use error::ErrorKind as EK;
 
-pub fn editor(config: Config) -> Result<Command> {
+
+/// Command enum for internal representation of different commands
+///
+enum CommandSpec { Editor, Pager }
+
+impl CommandSpec {
+    /// The canonical name of the program
+    ///
+    pub fn name(&self) -> &str {
+        match self {
+            &CommandSpec::Editor => "editor",
+            &CommandSpec::Pager  => "pager",
+        }
+    }
+
+    /// Get the name of the git config containing the program name or path
+    ///
+    pub fn config_name(&self) -> &str {
+        match self {
+            &CommandSpec::Editor => "core.editor",
+            &CommandSpec::Pager  => "core.pager",
+        }
+    }
+
+    /// Get the names of the environment variables which may contain the program
+    /// name
+    ///
+    pub fn env_var_names(&self) -> slice::Iter<'static, &'static str> {
+        match self {
+            &CommandSpec::Editor => {static X : &'static [&'static str] = &["GIT_EDITOR", "EDITOR"]; X},
+            &CommandSpec::Pager  => {static X : &'static [&'static str] = &["GIT_PAGER",  "PAGER" ]; X},
+        }.into_iter()
+    }
+}
+
+
+/// Build a `Command` from the config provided
+///
+/// A bare command will be returned containing
+///
+fn program(config: Config, command: CommandSpec) -> Result<Command> {
     config
-        .get_entry("core.editor")
-        .chain_err(|| EK::ConfigError("core.editor".to_owned()))?
+        .get_entry(command.config_name())
+        .chain_err(|| EK::ConfigError(command.config_name().to_owned()))?
         .value()
         .map(String::from)
-        .or_else(|| env_var("GIT_EDITOR").ok())
-        .or_else(|| env_var("EDITOR").ok())
+        .or_else(|| command.env_var_names().map(env_var).filter_map(|res| res.ok()).next())
         .map(Command::new)
-        .ok_or_else(|| Error::from(EK::ProgramError("editor".to_owned())))
+        .ok_or_else(|| EK::ProgramError(command.name().to_owned()).into())
+}
+
+
+pub fn editor(config: Config) -> Result<Command> {
+    program(config, CommandSpec::Editor)
+}
+
+pub fn pager(config: Config) -> Result<Command> {
+    program(config, CommandSpec::Pager)
 }
 
