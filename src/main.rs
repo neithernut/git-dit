e474ecd53f1005c72ef7874dd8d3d119025c3c8c
Julian Ganz <neither@nut.email>
2017-03-24 14:31:10 +0100

    Fix typo in subcommand implementation sectioning
    Really embarrassing typo.

--- src/main.rs
+++ src/main.rs
@@ -152,7 +152,7 @@ fn get_issue_tree_init_hashes(repo: &Repository, _: &clap::ArgMatches) -> i32 {
 }
 
 
-// Plumbing subcommand implementations
+// Porcelain subcommand implementations
 
 // ...
 
