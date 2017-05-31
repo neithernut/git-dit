6f5a80f9144651c23e0760929ed6a4d0e2d8d74b
Julian Ganz <neither@nut.email>
2017-05-31 17:22:10 +0200

    Add main library documentation

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -7,6 +7,32 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
+//! The git-dit library
+//!
+//! This library provides low-level functionality for accessing, creating and
+//! manipulating "git-dit" issues and messages. It is implemented on top of the
+//! `git2` crate. This librarie's documentation primarily provides information
+//! about its API and abstract processing of issues and messages.
+//!
+//!
+//! # Issues
+//!
+//! Issues are stored in git repositories. The issues availible in a repository
+//! may be accessed through the `RepositoryExt` extension trait implementation
+//! for `git2::Repository`.
+//!
+//! An issue is primarily a tree of messages, consisting of at least an initial
+//! message. An issue also has a "head reference". The head reference lets the
+//! maintainer indicate an "upstream status" of the issue, e.g. by pointing to a
+//! message which introduces a textual solution or a state.
+//!
+//! # Messages
+//!
+//! Like emails, messages are immutable once released to the public. Each
+//! message has an author and a creation date. Additionally, a message may
+//! contain arbitrary metadata in the form of git-tags.
+//!
+
 #[macro_use] extern crate error_chain;
 #[macro_use] extern crate lazy_static;
 extern crate git2;
