802740b7757d7bea60b6010337c04881206afd5d
Julian Ganz <neither@nut.email>
2017-07-06 20:19:19 +0200

    Explicitly set the markdown format to use
    ... we might want to tweak it at some point.

--- mkmanpage.rs
+++ mkmanpage.rs
@@ -4,6 +4,7 @@ fn main() {
     if std::env::var("BUILD_GIT_DIT_MAN").is_ok() {
         assert!(Command::new("pandoc")
             .arg("-s")
+            .arg("-f").arg("markdown")
             .arg("-t").arg("man")
             .arg("git-dit.1.md")
             .arg("-o").arg("git-dit.1")
