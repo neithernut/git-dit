09e2403fdf92695606d94667888098b908d03d7f
Julian Ganz <neither@nut.email>
2017-07-24 18:19:52 +0200

    Add Blocks iterator for iterating over lines extracting blocks

--- lib/src/message/block.rs
+++ lib/src/message/block.rs
@@ -17,6 +17,7 @@
 //!
 
 use message::trailer::Trailer;
+use std::str::FromStr;
 
 
 /// A block of lines
@@ -29,5 +30,81 @@ pub enum Block {
 }
 
 
-// TODO: iterator
+/// Iterate over blocks of lines instead of lines
+///
+/// This iterator wraps an iterator over lines and offers iteration over the
+/// blocks found in the sequence of lines. Paragraphs and blocks of text are
+/// cleanly separated from another.
+///
+#[derive(Debug)]
+pub struct Blocks<I, S>(I)
+    where I: Iterator<Item = S>,
+          S: AsRef<str>;
+
+impl<I, S> Iterator for Blocks<I, S>
+    where I: Iterator<Item = S>,
+          S: AsRef<str>
+{
+    type Item = Block;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut lines = Vec::new();
+        let mut trailers: Vec<Trailer> = Vec::new();
+        let mut is_trailer = true;
+
+        // get us the next block
+        for line in &mut self.0 {
+            let trimmed = line.as_ref().trim_right();
+
+            // If we encounter an empty line, we are done. However, we should
+            // refrain from reporting empty blocks.
+            if trimmed.is_empty() {
+                if lines.is_empty() {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            // Even if we encountered only trailers in the current block, we
+            // keep all the lines. We might need them in case the block turns
+            // out to be a paragraph.
+            lines.push(trimmed.to_string());
+
+            // Parsing trailers is far more expensive than accumulating strings.
+            if !is_trailer {
+                continue;
+            }
+
+            if trimmed.starts_with(" ") {
+                // We encountered a part of a multiline trailer.
+                if let Some(ref mut trailer) = trailers.last_mut() {
+                    trailer.value.append(trimmed);
+                } else {
+                    // Turns out this is a paragraph with the first line being
+                    // indented.
+                    is_trailer = false;
+                }
+            } else if let Ok(trailer) = Trailer::from_str(trimmed) {
+                // This looks like a trailer.
+                trailers.push(trailer);
+            } else {
+                // It's just text.
+                is_trailer = false;
+            }
+        }
+
+        // If we did not encounter any lines at all, we must have run out of
+        // lines.
+        if lines.is_empty() {
+            return None;
+        }
+
+        if is_trailer {
+            Some(Block::Trailer(trailers))
+        } else {
+            Some(Block::Text(lines))
+        }
+    }
+}
 
