c27579915dc41f3848844d34d9ee997fdfa14b26
Julian Ganz <neither@nut.email>
2017-08-17 17:16:58 +0200

    Use the Issue::id() function instead of accessing the id directly
    This introduces greater flexibility.

--- lib/src/issue.rs
+++ lib/src/issue.rs
@@ -130,7 +130,7 @@ impl<'r> Issue<'r> {
     /// Get the issue's initial message
     ///
     pub fn initial_message(&self) -> Result<git2::Commit<'r>> {
-        self.repo.find_commit(self.id).chain_err(|| EK::CannotGetCommit)
+        self.repo.find_commit(self.id()).chain_err(|| EK::CannotGetCommit)
     }
 
     /// Get possible heads of the issue
@@ -142,7 +142,7 @@ impl<'r> Issue<'r> {
         let glob = format!("**/dit/{}/head", self.ref_part());
         self.repo
             .references_glob(&glob)
-            .chain_err(|| EK::CannotFindIssueHead(self.id))
+            .chain_err(|| EK::CannotFindIssueHead(self.id()))
     }
 
     /// Get the local issue head for the issue
@@ -154,7 +154,7 @@ impl<'r> Issue<'r> {
         let refname = format!("refs/dit/{}/head", self.ref_part());
         self.repo
             .find_reference(&refname)
-            .chain_err(|| EK::CannotFindIssueHead(self.id))
+            .chain_err(|| EK::CannotFindIssueHead(self.id()))
     }
 
     /// Get local references for the issue
@@ -307,19 +307,19 @@ impl<'r> Issue<'r> {
     /// part after the  `dit/`.
     ///
     pub fn ref_part(&self) -> String {
-        self.id.to_string()
+        self.id().to_string()
     }
 }
 
 impl<'r> fmt::Display for Issue<'r> {
     fn fmt(&self, f: &mut fmt::Formatter) -> RResult<(), fmt::Error> {
-        write!(f, "{}", self.id)
+        write!(f, "{}", self.id())
     }
 }
 
 impl<'r> PartialEq for Issue<'r> {
     fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
+        self.id() == other.id()
     }
 }
 
@@ -329,7 +329,7 @@ impl<'r> hash::Hash for Issue<'r> {
     fn hash<H>(&self, state: &mut H)
         where H: hash::Hasher
     {
-        self.id.hash(state);
+        self.id().hash(state);
     }
 }
 
