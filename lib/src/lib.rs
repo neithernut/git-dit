ca3430edc0ab79003a65cc901f18e3e5ffc302f6
Julian Ganz <neither@nut.email>
2017-08-26 19:49:46 +0200

    Move recursion limit directive below the license header
    The file should start with a license header.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,4 +1,3 @@
-#![recursion_limit = "1024"]
 // git-dit - the distributed issue tracker for git
 // Copyright (C) 2016, 2017 Matthias Beyer <mail@beyermatthias.de>
 // Copyright (C) 2016, 2017 Julian Ganz <neither@nut.email>
@@ -7,6 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
+#![recursion_limit = "1024"]
 
 //! The git-dit library
 //!
