bc38ec4b2da9a9f3dd227ad47881a0e4fe591656
Julian Ganz <neither@nut.email>
2017-08-28 22:25:54 +0200

    Add FormattedLines iterator adapter for formatting

--- src/display/formatter.rs
+++ src/display/formatter.rs
@@ -92,3 +92,106 @@ impl<T, B> Borrow<T> for BorrowHelper<T, B>
     }
 }
 
+
+/// Adapter iterator for transforming formatting tokens to lines of text
+///
+/// This adapter wraps an iterator over formatting tokens, bundled with an item
+/// to format.
+/// All `Expandable` tokens returned by the wrapped iterator will be expanded
+/// recursively.
+/// The iterator will construct lines from the resulting "simple" tokens, which
+/// will be returned as items.
+///
+pub struct FormattedLines<I, J, T, K, B>
+    where I: Iterator<Item = J>, // Underlying token iterator
+          J: Borrow<FormattingToken<T, K>>, // Tokens returned by the iterator
+          T: TokenExpander<Item = K>, // The specific token expander
+          // K: Item to format
+          B: Borrow<K> // Reference of item to format
+{
+    inner: I,
+    item: B,
+    tokenstack: Vec<BorrowHelper<FormattingToken<T, K>, J>>,
+}
+
+impl<I, J, T, K, B> FormattedLines<I, J, T, K, B>
+    where I: Iterator<Item = J>,
+          J: Borrow<FormattingToken<T, K>>,
+          T: TokenExpander<Item = K>,
+          B: Borrow<K>
+{
+    pub fn new<A>(inner: A, item: B) -> Self
+        where A: IntoIterator<Item = J, IntoIter = I>
+    {
+        FormattedLines {
+            inner: inner.into_iter(),
+            tokenstack: Vec::new(),
+            item: item
+        }
+    }
+}
+
+impl<I, J, T, K, B> Iterator for FormattedLines<I, J, T, K, B>
+    where I: Iterator<Item = J>,
+          J: Borrow<FormattingToken<T, K>>,
+          T: TokenExpander<Item = K>,
+          B: Borrow<K>
+{
+    type Item = Result<String, T::Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Implementers' note: in case you did not guess by now, this adapter
+        // is implemented as a stack machine. As described in the user
+        // implementation, "simple" tokens are used for composing lines.
+        // `Expandable` tokens are expanded to new tokens via the build-in
+        // expansion mechanism.
+        // Since those tokens may contain line-ends, we have have to buffer
+        // them. Since they may contain other `Expandable` tokens, we cannot
+        // simply store the returned list but put them on the `tokenstack`.
+        // From that stack, we pop an item and process it, which may result in
+        // more expanded items on the stack.
+        let mut line: Option<String> = None;
+        loop {
+            // Retrieve a token. Check the stack first before consulting the
+            // inner iterator.
+            let token = match self
+                .tokenstack
+                .pop()
+                .or_else(|| self.inner.next().map(BorrowHelper::Borrowed))
+            {
+                Some(val)   => val,
+                None        => break,
+            };
+
+            match token.borrow() {
+                &FormattingToken::Expandable(ref expander, _) => match expander
+                    .expand_token(self.item.borrow())
+                {
+                    Ok(mut tokens) => {
+                        // Put the tokens on the stack, in reverse order since we are popping
+                        // items rather than iterating over them.
+                        tokens.reverse();
+                        self.tokenstack
+                            .extend(tokens.into_iter().map(BorrowHelper::Value));
+                    },
+                    Err(err) => return Some(Err(err)),
+                },
+                &FormattingToken::Text(ref text) => {
+                    // Expand the "simple" token.
+                    line = Some(line.take().unwrap_or_default() + text);
+                },
+                &FormattingToken::LineEnd => {
+                    // Make sure we return a line one each line end, even if
+                    // it's empty.
+                    if line.is_none() {
+                        line = Some(String::new())
+                    }
+                    // Return the line
+                    break
+                },
+            }
+        }
+        line.map(|l| Ok(l))
+    }
+}
+
